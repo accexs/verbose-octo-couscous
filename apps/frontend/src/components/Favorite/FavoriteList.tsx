e07244112e984bc72ae63d856b7ccdfeaf3b4e5b
Ronny Arvelo <ronny.arvelo@gmail.com>
2024-02-05 08:07:07 -0300

    fix on build

--- apps/frontend/src/components/Favorite/FavoriteList.tsx
+++ apps/frontend/src/components/Favorite/FavoriteList.tsx
@@ -1,14 +1,19 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import { getFavorites, isIdInFavorites } from "@/services/Favorite";
+import { getFavorites } from "@/services/Favorite";
 
 const FavoriteList: React.FC = () => {
-  const [favorites, setFavorites] = useState(getFavorites());
+  const [favorites, setFavorites] = useState<string[]>([]);
+
+  useEffect(() => {
+    const allFavorites = getFavorites();
+    setFavorites(allFavorites);
+  }, []);
 
   return (
     <div>
       <h2>This are your favorites</h2>
-      {getFavorites().map((favorite, index) => {
+      {favorites.map((favorite, index) => {
         return <div key={index}>{favorite}</div>;
       })}
     </div>
